430a7c50c22fdda2381f4f88389d7af20ff130e0
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-06-01 17:33:20 -0400

    commit tree 72fbc3832ce73951c5e244ccf916fb49449bd3f5

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name , age , sex , color , temperament , alive) VALUES ("Mr. Chocolate" , 5 , "M" , "brown" , "mean" , 0);
+INSERT INTO bears (name , age , sex , color , temperament , alive) VALUES ("Rowdy" , 3 , "F" , "green" , "nice" , 1 );
+INSERT INTO bears (name , age , sex , color , temperament , alive) VALUES ("Tabitha" , 6 , "M" , "purple" , "tired" , 1 );
+INSERT INTO bears (name , age , sex , color , temperament , alive) VALUES ("Sergeant Brown" , 11 , "F" , "yellow" , "sleepy" , 0);
+INSERT INTO bears (name , age , sex , color , temperament , alive) VALUES ("Melissa" , 22 , "M" , "orange" , "nice" , 1);
+INSERT INTO bears (name , age , sex , color , temperament , alive) VALUES ("Grinch" , 1 , "F" , "green" , "nice" , 1);
+INSERT INTO bears (name , age , sex , color , temperament , alive) VALUES ("Wendy" , 13 , "F" , "brown" , "mean" , 1);
+INSERT INTO bears (name , age , sex , color , temperament , alive) VALUES (NULL , 4 , "F" , "tabby" , "mean" , 1);
